b11f9442cbe9c362f7d2e198855d3e0b766bc420
Guillaume HANOTEL <guillaumehanotel@orange.fr>
2020-03-06 12:13:12 +0100

    GREEN should print 'FizzBuzz'

--- src/Kata.php
+++ src/Kata.php
@@ -3,7 +3,9 @@
 class Kata {
 
     public function fizzBuzz(int $x): string {
-        if($x % 3 == 0) {
+        if($x % 3 == 0 && $x % 5 == 0){
+            return 'FizzBuzz';
+        } else if($x % 3 == 0) {
             return 'Fizz';
         } else if($x % 5 == 0) {
             return 'Buzz';
