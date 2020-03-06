9454b1f448a3db2236b270898437eef61af82ee9
Guillaume HANOTEL <guillaumehanotel@orange.fr>
2020-03-06 11:51:50 +0100

    GREEN should print ''

--- src/Kata.php
+++ src/Kata.php
@@ -3,8 +3,14 @@
 class Kata {
 
     public function fizzBuzz(int $x): string {
-
+        if($x % 3 == 0) {
+            return 'Fizz';
+        } else if($x % 5 == 0) {
+            return 'Buzz';
+        } else if($x % 3 == 0 && $x % 5 == 0) {
+            return 'FizzBuzz';
+        } else {
+            return '';
+        }
     }
-
-
 }
--- tests/KataTest.php
+++ tests/KataTest.php
@@ -9,14 +9,15 @@ class KataTest extends TestCase {
 
     private static $kata;
 
-    public function __construct() {
+    public function __construct($name = null, array $data = [], $dataName = '') {
+        parent::__construct($name, $data, $dataName);
         self::$kata = new Kata();
     }
 
     public function testShouldReturnEmpty() {
         $input = 1;
         $expectedResult = '';
-        $this->assertSame($expectedResult, self::$kata->fizzBuzz($input));
+        $this->assertEquals($expectedResult, self::$kata->fizzBuzz($input));
     }
 
 }
