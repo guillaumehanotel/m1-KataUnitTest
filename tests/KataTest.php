f37b9a4a980c146e47f3f274a73a4a4488b21789
Guillaume HANOTEL <guillaumehanotel@orange.fr>
2020-03-06 11:45:45 +0100

    RED should print ''

--- tests/KataTest.php
+++ tests/KataTest.php
@@ -3,7 +3,20 @@
 use PHPUnit\Framework\TestCase;
 
 require './vendor/autoload.php';
+require './src/Kata.php';
 
 class KataTest extends TestCase {
+
+    private static $kata;
+
+    public function __construct() {
+        self::$kata = new Kata();
+    }
+
+    public function testShouldReturnEmpty() {
+        $input = 1;
+        $expectedResult = '';
+        $this->assertSame($expectedResult, self::$kata->fizzBuzz($input));
+    }
 
 }
