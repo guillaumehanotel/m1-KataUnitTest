0f81932e075b0c5c9b3c68d31fd5e6ea6160dbbb
Guillaume HANOTEL <guillaumehanotel@orange.fr>
2020-03-06 12:14:46 +0100

    REFACTOR should print 'FizzBuzz'

--- src/Kata.php
+++ src/Kata.php
@@ -3,14 +3,9 @@
 class Kata {
 
     public function fizzBuzz(int $x): string {
-        if($x % 3 == 0 && $x % 5 == 0){
-            return 'FizzBuzz';
-        } else if($x % 3 == 0) {
-            return 'Fizz';
-        } else if($x % 5 == 0) {
-            return 'Buzz';
-        } else {
-            return '';
-        }
+        $output = '';
+        $output .= $x % 3 == 0 ? 'Fizz' : '';
+        $output .= $x % 5 == 0 ? 'Buzz' : '';
+        return $output;
     }
 }
